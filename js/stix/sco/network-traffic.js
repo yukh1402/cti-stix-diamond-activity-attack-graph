9367c6b614943f644d1604f9b44710d312914f51
Yusuf Khan <yusuf.khan@mni.thm.de>
2021-05-07 19:01:10 +0200

    Networktraffic type missing

--- js/stix/sco/network-traffic.js
+++ js/stix/sco/network-traffic.js
@@ -1,4 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NETWORK_TRAFFIC_TYPE = void 0;
-exports.NETWORK_TRAFFIC_TYPE = "network-traffic";
+export const NETWORK_TRAFFIC_TYPE = "network-traffic";
